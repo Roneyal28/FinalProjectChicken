40e414287153161ecc9472455cd720e679666c7e
Roneyal28 <roneyal28@gmail.com>
2026-07-29 22:02:38 +0300

    camera fix

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -1,33 +1,82 @@
-using System;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
     public GameObject target;
 
-   [SerializeField] float speed = 1f; 
-   [SerializeField] float minX;
-   [SerializeField] float maxX;
-   [SerializeField] float minY;
-   [SerializeField] float maxY;
-   Vector3 targetCamPos;
+    [Header("Follow")]
+    [SerializeField] float speed = 1f;
 
-    private float CamZ;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Barn Limits")]
+    [Tooltip("Left edge of the area the camera can show.")]
+    [SerializeField] float minX;
+    [Tooltip("Right edge of the area the camera can show.")]
+    [SerializeField] float maxX;
+    [Tooltip("Bottom edge of the area the camera can show.")]
+    [SerializeField] float minY;
+    [Tooltip("Top edge of the area the camera can show.")]
+    [SerializeField] float maxY;
+
+    Vector3 targetCamPos;
+
+    private Camera followCamera;
+    private float camZ;
+
     void Awake()
     {
-        CamZ = transform.position.z;
+        followCamera = GetComponent<Camera>();
+        camZ = transform.position.z;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        /*
+        if (target == null)
+        {
+            return;
+        }
+
         targetCamPos = target.transform.position;
-        targetCamPos.x = math.clamp(targetCamPos.x, minX, maxX);
-        targetCamPos.y = math.clamp(targetCamPos.y, minY, maxY);*/
-        transform.position = Vector3.Lerp(transform.position, target.transform.position, Time.deltaTime * speed);
-        transform.position = new Vector3(transform.position.x, transform.position.y, CamZ);
+        ClampCameraPosition(ref targetCamPos);
+        targetCamPos.z = camZ;
+
+        Vector3 newCameraPosition = Vector3.Lerp(transform.position, targetCamPos, Time.deltaTime * speed);
+        ClampCameraPosition(ref newCameraPosition);
+        newCameraPosition.z = camZ;
+
+        transform.position = newCameraPosition;
+    }
+
+    private void ClampCameraPosition(ref Vector3 cameraPosition)
+    {
+        float halfHeight = 0f;
+        float halfWidth = 0f;
+
+        if (followCamera != null && followCamera.orthographic)
+        {
+            halfHeight = followCamera.orthographicSize;
+            halfWidth = halfHeight * followCamera.aspect;
+        }
+
+        float cameraMinX = minX + halfWidth;
+        float cameraMaxX = maxX - halfWidth;
+        float cameraMinY = minY + halfHeight;
+        float cameraMaxY = maxY - halfHeight;
+
+        if (cameraMinX > cameraMaxX)
+        {
+            float centerX = (minX + maxX) * 0.5f;
+            cameraMinX = centerX;
+            cameraMaxX = centerX;
+        }
+
+        if (cameraMinY > cameraMaxY)
+        {
+            float centerY = (minY + maxY) * 0.5f;
+            cameraMinY = centerY;
+            cameraMaxY = centerY;
+        }
+
+        cameraPosition.x = Mathf.Clamp(cameraPosition.x, cameraMinX, cameraMaxX);
+        cameraPosition.y = Mathf.Clamp(cameraPosition.y, cameraMinY, cameraMaxY);
     }
 }
